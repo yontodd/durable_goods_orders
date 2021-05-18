6f735e43882e7876d87a8f201fc46a29e60edde6
yontodd <57724970+yontodd@users.noreply.github.com>
2021-05-18 18:22:40 -0500

    Update durable_goods_orders.py

--- durable_goods_orders.py
+++ durable_goods_orders.py
@@ -1,34 +1,60 @@
 # -*- coding: utf-8 -*-
+"""
+Created on Tue May 18 18:09:45 2021
+
+@author: jonat
+"""
 
 import requests
-import datetime
+from datetime import datetime
+import calendar
 import pandas as pd
 import numpy as np
 
-current_year = '2021'
-current_month = '03'
-current = current_year + "-" + current_month
+current_report_month = ()
 
-prior_month = ()
-prior_year = ()
-
-if current_month == 1:
-  prior_month = 12
+if datetime.now().month - 1 == 0:
+  current_report_month = 12
 else:
-  prior_month = str((int(current_month) - 1)).zfill(2)
+  current_report_month = datetime.now().month - 1
 
-if current_month == 1:
-  prior_year = str((int(current_year) - 1))
+current_report_month = str(current_report_month).zfill(2)
+
+current_report_year = ()
+
+if current_report_month == 12:
+  current_report_year = datetime.now().year -1
 else:
-  prior_year = (current_year)
+  current_report_year = datetime.now().year
 
-prior = prior_year + '-' + prior_month
+current_report_year = str(current_report_year)
+
+current = current_report_year + "-" + current_report_month
+
+previous_report_month = ()
+
+if int(current_report_month) == 1:
+  previous_report_month = 12
+else:
+  previous_report_month = int(current_report_month) - 1
+
+previous_report_month = str(previous_report_month).zfill(2)
+
+previous_report_year = ()
+
+if current_report_month == 12:
+  previous_report_year = int(current_report_year - 1)
+else:
+  previous_report_year = current_report_year
+
+previous_report_year = str(previous_report_year)
+
+previous = previous_report_year + "-" + previous_report_month
 
 print("Current month: {}".format(current))
-print("Prior month: {}".format(prior))
+print("Prior month: {}".format(previous))
 
-base_url = "https://api.census.gov/data/timeseries/eits/advm3?get=data_type_code,time_slot_id,seasonally_adj,category_code,cell_value,error_data&for=us:*&time=from+{}".format(prior)
-
+base_url = "https://api.census.gov/data/timeseries/eits/advm3?get=data_type_code,time_slot_id,seasonally_adj,category_code,cell_value,error_data&for=us:*&time=from+{}".format(previous)
 response = requests.get(base_url)
 data = response.json()
 df = pd.DataFrame(data[1:], columns = data[0])
@@ -50,4 +76,3 @@ dg_data = df[(df.data_type_code.isin(type_variables)) &
 durable_goods_orders = pd.merge(zip_list, dg_data, how = 'left', left_on = ["data_type_code","category_code"], right_on = ["data_type_code","category_code"])
 
 durable_goods_orders
-
